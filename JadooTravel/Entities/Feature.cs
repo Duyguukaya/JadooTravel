ff9cbe2f67bea87887b80a748c41f740c66797cb
duygukayaa <dygkaya56@gmail.com>
2025-09-17 21:59:53 +0300

    partialview ve dto işlemleri yapıldı

--- JadooTravel/Entities/Feature.cs
+++ JadooTravel/Entities/Feature.cs
@@ -1,7 +1,12 @@
-﻿namespace JadooTravel.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace JadooTravel.Entities
 {
     public class Feature
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string FeatureId { get; set; }
         public string Tittle { get; set; }
         public string MainTittle { get; set; }
