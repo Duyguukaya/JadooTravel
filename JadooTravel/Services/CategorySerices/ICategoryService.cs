45630fd11e6d2ea0f6acf96159117ab8961343e8
Duyguukaya <Duyguukaya@users.noreply.github.com>
2025-09-23 22:02:13 +0300

    ...

---
+++ JadooTravel/Services/CategorySerices/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using JadooTravel.Dtos.CategoryDtos;
+
+namespace JadooTravel.Services.CategorySerices
+{
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategoryAsync(string id);
+        Task<GetCategoryByIdDto> GetCategoryByIdAsync(string id);
+    }
+}
