d7515120262de3128840d779e23e331ca586f828
Duyguukaya <Duyguukaya@users.noreply.github.com>
2025-09-30 14:58:19 +0300

    dil desteği eklendi.

---
+++ JadooTravel/Controllers/CultureController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JadooTravel.Controllers
+{
+    public class CultureController : Controller
+    {
+
+        [HttpPost]
+        public IActionResult SetCulture(string culture, string returnUrl)
+        {
+            if (string.IsNullOrEmpty(culture))
+                culture = "tr-TR"; // default dil
+
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+
+            return LocalRedirect(returnUrl ?? "/");
+        }
+    }
+}
